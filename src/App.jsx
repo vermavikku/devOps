d279afbebc997397835fb1710392168900336f5b
Vikas Verma <vermavikkudeveloper@gmail.com>
2024-09-01 11:57:46 +0530

    updated name

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vikas Verma</h1>
+      <h1>Vikas Bhai Verma</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
